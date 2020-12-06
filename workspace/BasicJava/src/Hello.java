77ca758f570df43573728e42d87b87d616d3502e
ISSAC7852 <su951100@gmail.com>
2020-12-06 20:48:51 +0900

    과제 1/4 20201206 java 게임만들기

--- workspace/BasicJava/src/Hello.java
+++ workspace/BasicJava/src/Hello.java
@@ -1,31 +1,12 @@
-import java.util.Arrays;
+import java.util.Scanner;
 
 
 public class Hello {
 
 	public static void main(String[] args) {
-		
+		long i =1234567890%4;
+		System.out.println(i);
 
-	int[] arr = new int[10];
-		
-		for(int i = 0; i <arr.length; i++){
-			arr [i] = (int)(Math.random()*100)+1;
-			
-		}
-		System.out.println(Arrays.toString(arr));
-		for(int i = 0; i <arr.length-1; i++){
-		int min = i;
-		for(int j = i+1; j < arr.length; j++){
-			if(arr[min]>arr[j]){
-				min = j;
-				
-			}
-		}
-				int temp = arr[i]; 
-				arr[i] = arr[min];
-				arr[min] = temp;
-		
-	}System.out.println(Arrays.toString(arr));
 	}
 
 }
