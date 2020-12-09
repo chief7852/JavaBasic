5abc7d419d32f63e551b1f560ae4ec23975880d5
ISSAC7852 <su951100@gmail.com>
2020-12-09 13:22:45 +0900

    2020-12-08 JAVA 프로그래머스 문제 풀이 못올린거

--- workspace/Livecoding/src/programers/knum.java
+++ workspace/Livecoding/src/programers/knum.java
@@ -3,7 +3,15 @@ package programers;
 public class knum {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[][] commands;
+		int[] array = {1,5,2,6,3,7,4};
+		int a=2;
+		int b=5;
+		int k=3;
+		array[2]~a
+		for(int i = a ; i <array.length;i++){
+			
+		}
 
 	}
 
