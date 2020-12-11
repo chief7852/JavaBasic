9ca026f24be602a920661b927cc816708390bc63
ISSAC7852 <su951100@gmail.com>
2020-12-11 21:01:15 +0900

    2020-12-11 java basic 오류 찾기 와 finally , try catch, 프로그래머슷

--- workspace/Livecoding/src/programers/endless.java
+++ workspace/Livecoding/src/programers/endless.java
@@ -1,44 +1,46 @@
 package programers;
 
+import java.util.Arrays;
+
 
 
 
 public class endless {
 
 	public static void main(String[] args) {
-		String[] participant = {"leo", "kiki", "eden"} ;
-		String[] completion = {"eden","kiki"};
+		String[] participant = {"marina", "josipa", "nikola", "vinko", "filipa"} ;
+		String[] completion = { "josipa", "filipa", "marina", "nikola"};
 		String answer = "";
-		
-		for(int i = 0; i <completion.length;i++){
-	        	for(int j = 0; j <participant.length;j++){
-	        		if(j == participant.length)
-	      
-	        			j= j-1;
-	        
-	        		if(participant[i]!=completion[j])
-	        		{
-	        			answer= participant[i];
-	        		}
-	        	}
-	        }
-		System.out.println(answer);
+		Arrays.sort(participant);
+		Arrays.sort(completion);
+		for(int i = 0; i <completion.length; i++)
+		{
+			if(participant[i] != completion[i])
+			{
+				answer = participant[i];
+			}
+		}
+		if(answer.equals(""))
+		{
+			answer = participant[participant.length-1];
+		}
+		 
+	System.out.println(answer);	
 	}
-
 }
 
-
-class Solution {
-    public String solution(String[] participant, String[] completion) {
-        String answer = "";
-        for(int i = 0; i <participant.length;i++){
-        	for(int j = 0; j <completion.length;j++){
-        		if(participant[i]!=completion[j])
-        		{
-        			answer= participant[i];
-        		}
-        	}
-        }
-        return answer;
-    }
-}
+//
+//class Solution {
+//    public String solution(String[] participant, String[] completion) {
+//        String answer = "";
+//        for(int i = 0; i <participant.length;i++){
+//        	for(int j = 0; j <completion.length;j++){
+//        		if(participant[i]!=completion[j])
+//        		{
+//        			answer= participant[i];
+//        		}
+//        	}
+//        }
+//        return answer;
+//    }
+//}
